228612b1e253e429500bcc65510c2bc8c3565f89
bahrus <anderson.bruce.b@gmail.com>
2018-12-13 10:10:43 -0500

    init

---
+++ wc-info-base.js
@@ -0,0 +1,70 @@
+import { XtallatX } from 'xtal-latx/xtal-latx.js';
+import { define } from 'xtal-latx/define.js';
+export class WCInfoBase extends XtallatX(HTMLElement) {
+    constructor() {
+        super(...arguments);
+        this._href = null;
+        this._c = false;
+    }
+    static get is() { return 'wc-info-base'; }
+    static get observedAttributes() {
+        return super.observedAttributes.concat(['href']);
+    }
+    attributeChangedCallback(n, ov, nv) {
+        super.attributeChangedCallback(n, ov, nv);
+        switch (n) {
+            case 'href':
+                this._href = nv;
+        }
+        this.onPropsChange();
+    }
+    get href() {
+        return this._href;
+    }
+    set href(nv) {
+        this.attr('href', nv);
+    }
+    connectedCallback() {
+        this._c = true;
+        this.onPropsChange();
+    }
+    genAttr(attrib) {
+        return /* html */ `<dt>${attrib.label}</dt><dd>${attrib.description}</dd>`;
+    }
+    genAttrs(attribs) {
+        return /* html */ `<dl>${attribs.map(attrib => this.genAttr(attrib)).join('')}`;
+    }
+    genWCInfo(wc) {
+        return /* html */ `
+        <div class="WCInfo card">
+            <div class="WCLabel">${wc.label}</div>
+            <div class="WCDesc">${wc.description}</div>
+            <detail>
+                <summary>Attributes</summary>
+                ${this.genAttrs(wc.attributes)}
+            </detail> 
+        </div>`;
+    }
+    genWCInfos(wcs) {
+        return wcs.map(wc => this.genWCInfo(wc)).join('');
+    }
+    genWCSuite(wcSuite) {
+        return /* html*/ `
+            <div class="WCSuite">
+                ${this.genWCInfos(wcSuite.tags)}
+            </div>
+        `;
+    }
+    onPropsChange() {
+        if (this._disabled || !this._c || this._href === null)
+            return;
+        fetch(this._href).then(resp => {
+            resp.json().then(info => {
+                const wcInfo = info;
+                this.innerHTML = this.genWCSuite(wcInfo);
+            });
+        });
+    }
+}
+define(WCInfoBase);
+//# sourceMappingURL=wc-info-base.js.map
---
+++ wc-info-base.ts
@@ -0,0 +1,85 @@
+import {XtallatX} from 'xtal-latx/xtal-latx.js';
+import {define} from 'xtal-latx/define.js';
+
+export interface IInfo{
+    label: string,
+    description: string,
+}
+
+export interface IAttribInfo extends IInfo{
+}
+
+export interface IWCInfo extends IInfo{
+    attributes: IAttribInfo[];
+}
+export interface IWCSuiteInfo{
+    tags: IWCInfo[];
+}
+
+export class WCInfoBase extends XtallatX(HTMLElement){
+    static get is(){return 'wc-info-base';}
+
+    static get observedAttributes(){
+        return super.observedAttributes.concat(['href']);
+    }
+
+    attributeChangedCallback(n: string, ov: string, nv: string){
+        super.attributeChangedCallback(n, ov, nv);
+        switch(n){
+            case 'href':
+                this._href = nv;
+        }
+        this.onPropsChange();
+    }
+
+    _href: string | null =  null;
+    get href(){
+        return this._href;
+    }
+    set href(nv){
+        this.attr('href', nv!);
+    }
+    _c = false;
+    connectedCallback(){
+        this._c = true;
+        this.onPropsChange();
+    }
+    genAttr(attrib: IAttribInfo){
+        return /* html */`<dt>${attrib.label}</dt><dd>${attrib.description}</dd>`;
+    }
+    genAttrs(attribs: IAttribInfo[]){
+        return /* html */`<dl>${attribs.map(attrib => this.genAttr(attrib)).join('')}`;
+    }
+    genWCInfo(wc: IWCInfo){
+        return /* html */`
+        <div class="WCInfo card">
+            <div class="WCLabel">${wc.label}</div>
+            <div class="WCDesc">${wc.description}</div>
+            <detail>
+                <summary>Attributes</summary>
+                ${this.genAttrs(wc.attributes)}
+            </detail> 
+        </div>`;
+    }
+    genWCInfos(wcs: IWCInfo[]){
+        return wcs.map(wc => this.genWCInfo(wc)).join('');
+    }
+    genWCSuite(wcSuite: IWCSuiteInfo){
+        return /* html*/`
+            <div class="WCSuite">
+                ${this.genWCInfos(wcSuite.tags)}
+            </div>
+        `;
+    }
+    onPropsChange(){
+        if(this._disabled || !this._c || this._href === null) return;
+        fetch(this._href).then(resp =>{
+            resp.json().then(info =>{
+                const wcInfo = info as IWCSuiteInfo;
+                this.innerHTML = this.genWCSuite(wcInfo);
+            })
+        })
+    }
+} 
+
+define(WCInfoBase);
