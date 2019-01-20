66e1f25545f8920106bab2ee3612cb8c193e8d86
bahrus <anderson.bruce.b@gmail.com>
2019-01-20 13:11:06 -0500

    ...

--- wc-info-base.js
+++ wc-info-base.js
@@ -1,14 +1,125 @@
-import { XtallatX } from 'xtal-latx/xtal-latx.js';
 import { define } from 'xtal-latx/define.js';
+import { XtalElement } from 'xtal-element/xtal-element.js';
+import { init } from 'trans-render/init.js';
 const package_name = 'package-name';
-export class WCInfoBase extends XtallatX(HTMLElement) {
+function createTemplate(innerHTML) {
+    const template = document.createElement('template');
+    template.innerHTML = innerHTML;
+    return template;
+}
+const attribTemplate = createTemplate(
+/* html */ `
+    <dt></dt><dd></dd>
+`);
+const WCInfoTemplate = createTemplate(
+/* html */ `
+<section class="WCInfo card">
+    <header>
+        <div class="WCLabel"></div>
+        <div class="WCDesc"></div>
+    </header>
+    <details>
+        <summary>attributes</summary>
+        <dl></dl>
+    </details> 
+</section>`);
+const mainTemplate = createTemplate(
+/* html */ `
+    <header>
+        <mark></mark>
+        <nav>
+            <a target="_blank">⚙️</a>
+        </nav>
+    </header>
+`);
+export class WCInfoBase extends XtalElement {
     constructor() {
         super(...arguments);
         this._href = null;
         this._packageName = null;
         this._c = false;
+        // genAttr(attrib: IAttribInfo){
+        //     return /* html */`<dt>${attrib.label}</dt><dd>${attrib.description}</dd>`;
+        // }
+        // genAttrs(attribs: IAttribInfo[]){
+        //     return /* html */`<dl>${attribs.map(attrib => this.genAttr(attrib)).join('')}`;
+        // }
+        // genWCInfo(wc: IWCInfo){
+        //     return /* html */`
+        //     <section class="WCInfo card">
+        //         <header>
+        //             <div class="WCLabel">${wc.label}</div>
+        //             <div class="WCDesc">${wc.description}</div>
+        //         </header>
+        //         <details>
+        //             <summary>attributes</summary>
+        //             ${this.genAttrs(wc.attributes)}
+        //         </details> 
+        //     </section>`;
+        // }
+        // genWCInfos(wcs: IWCInfo[]){
+        //     return wcs.map(wc => this.genWCInfo(wc)).join('');
+        // }
+        // genWCSuite(wcSuite: IWCSuiteInfo){
+        //     return /* html*/`
+        //         <header>
+        //             <mark>${this._packageName}</mark>
+        //             <nav>
+        //                 <a href="${this._href}" target="_blank">⚙️</a>
+        //             </nav>
+        //         </header>
+        //         ${this.genWCInfos(wcSuite.tags)}
+        //     `;
+        // }
+        // onPropsChange(){
+        //     if(this._disabled || !this._c || this._href === null) return;
+        //     fetch(this._href).then(resp =>{
+        //         resp.json().then(info =>{
+        //             this.render(info as IWCSuiteInfo)
+        //         })
+        //     })
+        // }
+        // render(wcInfo: IWCSuiteInfo){
+        //     this.innerHTML = this.genWCSuite(wcInfo);
+        // }
     }
     static get is() { return 'wc-info-base'; }
+    get noShadow() {
+        return true;
+    }
+    get eventSwitchContext() {
+        return {};
+    }
+    get renderContext() {
+        return {
+            init: init,
+            transform: {
+                header: x => ({
+                    matchFirstChild: {
+                        mark: ({ target }) => this.packageName
+                    },
+                })
+            }
+        };
+    }
+    get ready() {
+        return this._href !== undefined && this._packageName !== undefined;
+    }
+    init(el) {
+        return new Promise(resolve => {
+            fetch(this._href).then(resp => {
+                resp.json().then(info => {
+                    resolve(info);
+                });
+            });
+        });
+    }
+    update(el) {
+        return this.init(el);
+    }
+    get mainTemplate() {
+        return mainTemplate;
+    }
     static get observedAttributes() {
         return super.observedAttributes.concat(['href', package_name]);
     }
@@ -37,55 +148,8 @@ export class WCInfoBase extends XtallatX(HTMLElement) {
         this.attr(package_name, nv);
     }
     connectedCallback() {
-        this._upgradeProperties(['href']);
-        this._c = true;
-        this.onPropsChange();
-    }
-    genAttr(attrib) {
-        return /* html */ `<dt>${attrib.label}</dt><dd>${attrib.description}</dd>`;
-    }
-    genAttrs(attribs) {
-        return /* html */ `<dl>${attribs.map(attrib => this.genAttr(attrib)).join('')}`;
-    }
-    genWCInfo(wc) {
-        return /* html */ `
-        <section class="WCInfo card">
-            <header>
-                <div class="WCLabel">${wc.label}</div>
-                <div class="WCDesc">${wc.description}</div>
-            </header>
-            <details>
-                <summary>attributes</summary>
-                ${this.genAttrs(wc.attributes)}
-            </details> 
-        </section>`;
-    }
-    genWCInfos(wcs) {
-        return wcs.map(wc => this.genWCInfo(wc)).join('');
-    }
-    genWCSuite(wcSuite) {
-        return /* html*/ `
-            <header>
-                <mark>${this._packageName}</mark>
-                <nav>
-                    <a href="${this._href}" target="_blank">⚙️</a>
-                </nav>
-            </header>
-            ${this.genWCInfos(wcSuite.tags)}
-            
-        `;
-    }
-    onPropsChange() {
-        if (this._disabled || !this._c || this._href === null)
-            return;
-        fetch(this._href).then(resp => {
-            resp.json().then(info => {
-                this.render(info);
-            });
-        });
-    }
-    render(wcInfo) {
-        this.innerHTML = this.genWCSuite(wcInfo);
+        this._upgradeProperties(['href', 'packageName']);
+        super.connectedCallback();
     }
 }
 define(WCInfoBase);
--- wc-info-base.ts
+++ wc-info-base.ts
@@ -1,5 +1,8 @@
 import {XtallatX} from 'xtal-latx/xtal-latx.js';
 import {define} from 'xtal-latx/define.js';
+import {XtalElement} from 'xtal-element/xtal-element.js';
+import {RenderContext} from 'trans-render/init.d.js';
+import {init} from 'trans-render/init.js';
 
 export interface IInfo{
     label: string,
@@ -17,8 +20,85 @@ export interface IWCSuiteInfo{
 }
 const package_name = 'package-name';
 
-export class WCInfoBase extends XtallatX(HTMLElement){
+function createTemplate(innerHTML: string) : HTMLTemplateElement{
+    const template = document.createElement('template') as HTMLTemplateElement;
+    template.innerHTML = innerHTML;
+    return template;
+}
+
+const attribTemplate = createTemplate(
+/* html */`
+    <dt></dt><dd></dd>
+`);
+
+const WCInfoTemplate = createTemplate(
+/* html */`
+<section class="WCInfo card">
+    <header>
+        <div class="WCLabel"></div>
+        <div class="WCDesc"></div>
+    </header>
+    <details>
+        <summary>attributes</summary>
+        <dl></dl>
+    </details> 
+</section>`);
+
+const mainTemplate = createTemplate(
+/* html */`
+    <header>
+        <mark></mark>
+        <nav>
+            <a target="_blank">⚙️</a>
+        </nav>
+    </header>
+`
+)
+
+export class WCInfoBase extends XtalElement<IWCSuiteInfo>{
     static get is(){return 'wc-info-base';}
+
+    get noShadow(){
+        return true;
+    }
+
+    get eventSwitchContext(){
+        return {};
+    }
+
+    get renderContext(){
+        return {
+            init: init,
+            transform:{
+                header: x => ({
+                    matchFirstChild: {
+                        mark: ({target}) => this.packageName
+                    },
+                })
+            }
+        } as RenderContext;
+    }
+
+    get ready(){
+        return this._href !== undefined && this._packageName !== undefined;
+    }
+
+    init(el: this){
+        return new Promise<IWCSuiteInfo>(resolve => {
+            fetch(this._href!).then(resp =>{
+                resp.json().then(info =>{
+                    resolve(info as IWCSuiteInfo);
+                })
+            })
+        })
+    }
+    update(el: this){
+        return this.init(el);
+    }
+
+    get mainTemplate(){
+        return mainTemplate;
+    }
 
     static get observedAttributes(){
         return super.observedAttributes.concat(['href', package_name]);
@@ -54,56 +134,55 @@ export class WCInfoBase extends XtallatX(HTMLElement){
     }
     _c = false;
     connectedCallback(){
-        this._upgradeProperties(['href']);
-        this._c = true;
-        this.onPropsChange();
+        this._upgradeProperties(['href', 'packageName']);
+        super.connectedCallback();
     }
-    genAttr(attrib: IAttribInfo){
-        return /* html */`<dt>${attrib.label}</dt><dd>${attrib.description}</dd>`;
-    }
-    genAttrs(attribs: IAttribInfo[]){
-        return /* html */`<dl>${attribs.map(attrib => this.genAttr(attrib)).join('')}`;
-    }
-    genWCInfo(wc: IWCInfo){
-        return /* html */`
-        <section class="WCInfo card">
-            <header>
-                <div class="WCLabel">${wc.label}</div>
-                <div class="WCDesc">${wc.description}</div>
-            </header>
-            <details>
-                <summary>attributes</summary>
-                ${this.genAttrs(wc.attributes)}
-            </details> 
-        </section>`;
-    }
-    genWCInfos(wcs: IWCInfo[]){
-        return wcs.map(wc => this.genWCInfo(wc)).join('');
-    }
-    genWCSuite(wcSuite: IWCSuiteInfo){
-        return /* html*/`
-            <header>
-                <mark>${this._packageName}</mark>
-                <nav>
-                    <a href="${this._href}" target="_blank">⚙️</a>
-                </nav>
-            </header>
-            ${this.genWCInfos(wcSuite.tags)}
+    // genAttr(attrib: IAttribInfo){
+    //     return /* html */`<dt>${attrib.label}</dt><dd>${attrib.description}</dd>`;
+    // }
+    // genAttrs(attribs: IAttribInfo[]){
+    //     return /* html */`<dl>${attribs.map(attrib => this.genAttr(attrib)).join('')}`;
+    // }
+    // genWCInfo(wc: IWCInfo){
+    //     return /* html */`
+    //     <section class="WCInfo card">
+    //         <header>
+    //             <div class="WCLabel">${wc.label}</div>
+    //             <div class="WCDesc">${wc.description}</div>
+    //         </header>
+    //         <details>
+    //             <summary>attributes</summary>
+    //             ${this.genAttrs(wc.attributes)}
+    //         </details> 
+    //     </section>`;
+    // }
+    // genWCInfos(wcs: IWCInfo[]){
+    //     return wcs.map(wc => this.genWCInfo(wc)).join('');
+    // }
+    // genWCSuite(wcSuite: IWCSuiteInfo){
+    //     return /* html*/`
+    //         <header>
+    //             <mark>${this._packageName}</mark>
+    //             <nav>
+    //                 <a href="${this._href}" target="_blank">⚙️</a>
+    //             </nav>
+    //         </header>
+    //         ${this.genWCInfos(wcSuite.tags)}
             
-        `;
-    }
-    onPropsChange(){
-        if(this._disabled || !this._c || this._href === null) return;
-        fetch(this._href).then(resp =>{
-            resp.json().then(info =>{
-                this.render(info as IWCSuiteInfo)
-            })
-        })
-    }
+    //     `;
+    // }
+    // onPropsChange(){
+    //     if(this._disabled || !this._c || this._href === null) return;
+    //     fetch(this._href).then(resp =>{
+    //         resp.json().then(info =>{
+    //             this.render(info as IWCSuiteInfo)
+    //         })
+    //     })
+    // }
 
-    render(wcInfo: IWCSuiteInfo){
-        this.innerHTML = this.genWCSuite(wcInfo);
-    }
+    // render(wcInfo: IWCSuiteInfo){
+    //     this.innerHTML = this.genWCSuite(wcInfo);
+    // }
 } 
 
 define(WCInfoBase);
