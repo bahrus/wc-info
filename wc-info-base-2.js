4156a1918ddfef718e64115457ec9595b464cb9b
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-06-20 15:22:56 -0400

    ...

--- wc-info-base-2.js
+++ wc-info-base-2.js
@@ -3,8 +3,13 @@ import { xc } from 'xtal-element/lib/XtalCore.js';
 import { xp } from 'xtal-element/lib/XtalPattern.js';
 import { define } from 'xtal-element/lib/define.js';
 import { DOMKeyPE } from 'xtal-element/lib/DOMKeyPE.js';
+import('proxy-prop/proxy-prop.js');
+import('xtal-fetch/xtal-fetch-get.js');
+import('pass-up/p-u.js');
 const mainTemplate = html `
-iah
+<proxy-prop from-host observe-prop=href to=[-href] ></proxy-prop>
+<xtal-fetch-get fetch -href></xtal-fetch-get>
+<p-u on=result-changed to-host prop=package val=target.result init-val=result></p-u>
 `;
 export class WCInfo extends HTMLElement {
     static is = 'wc-info';
@@ -23,13 +28,24 @@ export class WCInfo extends HTMLElement {
     onPropChange(n, prop, nv) {
         this.reactor.addToQueue(prop, nv);
     }
+    href;
+    package;
 }
 const propActions = [
     xp.manageMainTemplate,
     xp.createShadow
 ];
+const baseProp = {
+    async: true,
+    dry: true,
+};
+const strProp1 = {
+    ...baseProp,
+    type: String
+};
 const propDefMap = {
     ...xp.props,
+    href: strProp1,
 };
 const slicedPropDefs = xc.getSlicedPropDefs(propDefMap);
 xc.letThereBeProps(WCInfo, slicedPropDefs, 'onPropChange');
--- wc-info-base-2.ts
+++ wc-info-base-2.ts
@@ -3,8 +3,16 @@ import {xc, ReactiveSurface, IReactor, PropAction, PropDef, PropDefMap} from 'xt
 import {xp} from 'xtal-element/lib/XtalPattern.js';
 import {define} from 'xtal-element/lib/define.js';
 import { DOMKeyPE } from 'xtal-element/lib/DOMKeyPE.js';
+import {Package} from 'node_modules/custom-elements-manifest/schema.d.js';
+import {XtalFetchBasePropertiesIfc} from 'node_modules/xtal-fetch/types.d.js';
+import {IProxyPropProps} from 'node_modules/proxy-prop/types.d.js';
+import('proxy-prop/proxy-prop.js');
+import('xtal-fetch/xtal-fetch-get.js');
+import('pass-up/p-u.js');
 const mainTemplate = html`
-iah
+<proxy-prop from-host observe-prop=href to=[-href] ></proxy-prop>
+<xtal-fetch-get fetch -href></xtal-fetch-get>
+<p-u on=result-changed to-host prop=package val=target.result init-val=result></p-u>
 `;
 export class WCInfo extends HTMLElement implements ReactiveSurface{
     static is = 'wc-info';
@@ -22,14 +30,25 @@ export class WCInfo extends HTMLElement implements ReactiveSurface{
     onPropChange(n: string, prop: PropDef, nv: any){
         this.reactor.addToQueue(prop, nv);
     }
+    href: string | undefined;
+    package: Package | undefined;
 }
 const propActions = [
     xp.manageMainTemplate,
     xp.createShadow
 ] as PropAction[];
+const baseProp: PropDef = {
+    async: true,
+    dry: true,
+};
+const strProp1: PropDef = {
+    ...baseProp,
+    type: String
+};
 const propDefMap: PropDefMap<WCInfo> = {
     ...xp.props,
-}
+    href: strProp1,
+};
 const slicedPropDefs = xc.getSlicedPropDefs(propDefMap);
 xc.letThereBeProps(WCInfo, slicedPropDefs, 'onPropChange');
 define(WCInfo);
