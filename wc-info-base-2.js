2317abf08f23375d8f2318d75590f9ee0b5d5b5f
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-05 13:19:55 -0400

    ...

--- wc-info-base-2.js
+++ wc-info-base-2.js
@@ -1,6 +1,7 @@
 import { X } from 'xtal-element/lib/X.js';
 import { html } from 'xtal-element/lib/html.js';
-import('pass-prop/pass-prop.js');
+import('pass-prop/p-p.js');
+import('pass-prop/p-p-x.js');
 import('pass-down/p-d-x.js');
 import('carbon-copy/c-c.js');
 import('xtal-fetch/xtal-fetch-get.js');
@@ -10,6 +11,14 @@ import('lib-id/li-bid.js');
 import('if-diff/if-diff.js');
 import('nomodule/no-module.js');
 const mainTemplate = html `
+<p-p from-parent-or-host observe-prop=href to=[-href] m=1></p-p>
+<xtal-fetch-get fetch -href></xtal-fetch-get>
+<p-d-x on=result-changed to=[-list] val=target.result val-filter=$.modules.[*].declarations[?(@.tagName)] val-filter-script-id=filter-out-less-typed-version></p-d-x>
+<h1>Custom Elements</h1>
+<i-bid -list>
+    <custom-element-declaration></custom-element-declaration>
+</i-bid>
+
 <template id=class-member-inner-row>
     <td>{{name}}</td>
     <td>{{description}}</td>
@@ -18,13 +27,36 @@ const mainTemplate = html `
 
 <template id=custom-element-declaration>
     <hr>
-    <div>{{_tagName}}</div>
+    <h2>{{_tagName}}</h2>
     <div data-desc="{{description}}">{{description}}</div>
-
-    <p-p from-parent-or-host observe-prop=members to=[-iff] m=1></p-p>
-    <p-p from-parent-or-host observe-prop=members to=[-list] m=1></p-p>
+    
+    <p-p-x from-parent-or-host observe-prop=members val-filter="$[?(@.kind=='field')]" to=[-iff] m=1></p-p-x>
+    <p-p-x from-parent-or-host observe-prop=members val-filter="$[?(@.kind=='field')]" to=[-list] m=1></p-p-x>
     <if-diff -iff is-non-empty-array>
         <template>
+            <h3>Properties</h3>
+            <li-bid template-id=./class-member-inner-row render-at-start-of=tbody -list tag=tr>
+            </li-bid>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </template>
+    </if-diff>
+    
+    <p-p-x from-parent-or-host observe-prop=members val-filter="$[?(@.kind=='method')]" to=[-iff] m=1></p-p-x>
+    <p-p-x from-parent-or-host observe-prop=members val-filter="$[?(@.kind=='method')]" to=[-list] m=1></p-p-x>
+    <if-diff -iff is-non-empty-array>
+        <template>
+            <h3>Methods</h3>
             <!-- <i-bid -list>
                 <wc-info-member></wc-info-member>
             </i-bid> -->
@@ -47,26 +79,6 @@ const mainTemplate = html `
     </if-diff>
 </template>
 <c-c copy from-prev-sibling string-props='["name", "description", "kind", "_tagName"]' obj-props='["members"]' noshadow></c-c>
-
-<!-- <template id=wc-info-member>
-    <hr>
-    <div>{{kind}} name: {{name}}</div>
-    <div>Description: {{description}}</div>
-    <p-p from-upsearch=wc-info-member observe-prop=parameters to=[-iff] m=1></p-p>
-    <p-p from-upsearch=wc-info-member observe-prop=parameters to=[-list] m=1></p-p>
-    <if-diff -iff is-non-empty-array>
-        <template>
-            <details>
-                <summary>Parameters</summary>
-                
-                <i-bid -list>
-                    <wc-info-parameter></wc-info-parameter>
-                </i-bid>
-            </details>
-        </template>
-    </if-diff>
-</template>
-<c-c copy from-prev-sibling string-props='["name", "description", "kind"]' obj-props='["parameters"]' noshadow></c-c> -->
 
 <!-- <template id=wc-info-parameter>
     <hr>
@@ -97,13 +109,7 @@ const mainTemplate = html `
         return Object.values(tagNameToDeclaration);
     }
 </script>
-<p-p from-parent-or-host observe-prop=href to=[-href] m=1></p-p>
-<xtal-fetch-get fetch -href></xtal-fetch-get>
-<p-d-x on=result-changed to=[-list] val=target.result val-filter=$.modules.[*].declarations[?(@.tagName)] val-filter-script-id=filter-out-less-typed-version></p-d-x>
-<div>Custom Elements</div>
-<i-bid -list>
-    <custom-element-declaration></custom-element-declaration>
-</i-bid>
+
 `;
 export class WCInfoBase extends X {
 }
