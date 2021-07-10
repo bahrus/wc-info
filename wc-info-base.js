6a263d1e7fb827e682e2538bb2ad94cc8eb22cb2
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-10 18:49:09 -0400

    ...

--- wc-info-base.js
+++ wc-info-base.js
@@ -27,9 +27,9 @@ const mainTemplate = html `
     <dfn part=ce-description class=description>{{description}}</dfn>
 
     <!-- Attributes -->
-    <p-p-x from-parent-or-host observe-prop=_attributes to=[-iff] m=2></p-p-x>
+    <p-p-x from-parent-or-host observe-prop=_attributes to=if-diff.attribs[-iff] m=2></p-p-x>
     <p-p-x from-parent-or-host observe-prop=_attributes to=li-bid.attribs[-list] m=1></p-p-x>
-    <if-diff -iff is-non-empty-array and-media-matches="(min-width: 600px)" lazy-delayx=2000>
+    <if-diff class=attribs -iff is-non-empty-array and-media-matches="(min-width: 600px)" lazy-delayx=2000>
         <template>
             <li-bid class=attribs bind-to-tag-virtually template-id=innerTemplate render-at-start-of=[-repeat] -list tag=tr tag-attr='{"part":"cea-item", "class": "item"}'>
                 <template>
@@ -55,7 +55,7 @@ const mainTemplate = html `
             </table>
         </template>
     </if-diff>
-    <if-diff -iff is-non-empty-array and-media-matches="(max-width: 599px)" lazy-delayx=2000>
+    <if-diff class=attribs -iff is-non-empty-array and-media-matches="(max-width: 599px)" lazy-delayx=2000>
         <template>
             <li-bid class=attribs bind-to-tag-virtually template-id=innerTemplate render-at-start-of=[-repeat] -list tag=tr tag-attr='{"part":"cea-item", "class": "item"}'>
                 <template>
@@ -84,9 +84,9 @@ const mainTemplate = html `
     </if-diff> 
 
     <!-- Events -->
-    <p-p-x from-parent-or-host observe-prop=events to=[-iff] m=2></p-p-x>
+    <p-p-x from-parent-or-host observe-prop=events to=if-diff.events[-iff] m=2></p-p-x>
     <p-p-x from-parent-or-host observe-prop=events to=li-bid.events[-list] m=1></p-p-x>
-    <if-diff -iff is-non-empty-array and-media-matches="(min-width: 600px)" lazy-delayx=2000>
+    <if-diff class=events -iff is-non-empty-array and-media-matches="(min-width: 600px)" lazy-delayx=2000>
         <template>
             <li-bid class=events bind-to-tag-virtually template-id=innerTemplate render-at-start-of=[-repeat] -list tag=tr tag-attr='{"part":"cee-item", "class": "item"}'>
                 <template>
@@ -112,7 +112,7 @@ const mainTemplate = html `
             </table>
         </template>
     </if-diff>
-    <if-diff -iff is-non-empty-array and-media-matches="(max-width: 599px)" lazy-delayx=2000>
+    <if-diff class=events -iff is-non-empty-array and-media-matches="(max-width: 599px)" lazy-delayx=2000>
         <template>
             <li-bid class=events bind-to-tag-virtually template-id=innerTemplate render-at-start-of=[-repeat] -list tag=tr tag-attr='{"part":"cee-item", "class": "item"}'>
                 <template>
@@ -199,9 +199,9 @@ const mainTemplate = html `
     </if-diff>
 
     <!-- slots -->
-    <p-p-x from-parent-or-host observe-prop=slots to=[-iff] m=1></p-p-x>
+    <p-p-x from-parent-or-host observe-prop=slots to=if-diff.slots[-iff] m=1></p-p-x>
     <p-p-x from-parent-or-host observe-prop=slots to=li-bid.slots[-list] m=1></p-p-x>
-    <if-diff -iff is-non-empty-array lazy-delayx=2000>
+    <if-diff class=slots -iff is-non-empty-array lazy-delayx=2000>
         <template>
             <li-bid class=slots bind-to-tag-virtually template-id=innerTemplate render-at-start-of=[-repeat] -list tag=tr tag-attr='{"part":"ces-item", "class": "item"}'>
                 <template>
