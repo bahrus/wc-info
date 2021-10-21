34e1bf60680a6fb34ccae3cc3171d9d7a6313a76
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-21 17:08:59 -0400

    0.0.126

--- wc-info-base2.ts
+++ wc-info-base2.ts
@@ -5,6 +5,7 @@ import ('be-reformable/be-reformable.js');
 import ('be-observant/be-observant.js');
 import ('./be-reformable-as-wc-info.js');
 import ('be-hive/be-hive.js');
+import ('be-repeated/be-repeated.js');
 
 if(document.querySelector('be-hive')===null){
     document.body.appendChild(document.createElement('be-hive'));
@@ -15,11 +16,22 @@ const mainTemplate = html`
 <h2 part=package>Package: <label></label></h2>
 <form be-reformable-as-wc-info='{
     "autoSubmit": true,
+    "emitEvents": ["declarations"],
     "url": {
         "onSet": "href",
         "vft": "href"
-    }
+    },
+    "debug": true
 }'></form>
+<template be-repeated='{
+    "transform": {
+
+    },
+    "list": {"observe": "form", "vft": "declarations", "fromProxy": "reformable-as-wc-info"},
+    "debug": true
+}'>
+    <custom-element-declaration></custom-element-declaration>
+</template>
 <be-hive></be-hive>
 `;
 const beDefinitiveProps: DefineArgs = {
