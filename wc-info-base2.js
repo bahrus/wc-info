4843845886086668836b28cdda92b4d88b1c5021
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-21 10:08:45 -0400

    0.0.126

--- wc-info-base2.js
+++ wc-info-base2.js
@@ -1,11 +1,25 @@
 import { html } from 'trans-render/lib/html.js';
 import 'be-definitive/be-definitive.js';
+import 'be-reformable/be-reformable.js';
+import 'be-observant/be-observant.js';
 const mainTemplate = html `
 <h1 part=title>Custom Elements API</h1>
+<h2 part=package>Package: <label></label></h2>
+<form be-reformable='{
+    "autoSubmit": true,
+    "url": {
+        "onSet": "href",
+        "vft": "href"
+    }
+}'></form>
+<be-reformable></be-reformable>
 `;
 const beDefinitiveProps = {
     config: {
-        tagName: 'wc-info-base'
+        tagName: 'wc-info-base',
+        propDefaults: {
+            href: ''
+        }
     }
 };
 mainTemplate.setAttribute('be-definitive', JSON.stringify(beDefinitiveProps));
